989dd2f045563a2394fbf8275a21dede6c261b1f
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-07-03 12:30:04 +0530

    Added logic to bounce back ball when it hits the paddle.

--- ping-pong/index.js
+++ ping-pong/index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Lets get the ball element
   let ball = document.getElementById("ball");
   let table = document.getElementById("table");
+  let paddle = document.getElementById("paddle");
 
   let ballX = 10; // Distance from left of the table
   let ballY = 10; // Distance from top of the table
@@ -24,6 +25,13 @@ document.addEventListener("DOMContentLoaded", () => {
     //   dy *= -1;
     // }
 
+    if (
+      ballX < paddle.offsetLeft + paddle.offsetWidth &&
+      ballY > paddle.offsetTop &&
+      ballY - ball.offsetHeight < paddle.offsetTop + paddle.offsetHeight
+    ) {
+      dx *= -1;
+    }
     if (ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) {
       // To get width and height of dynamic changes in syles to handle for all cases instead of hardcoding
       dx *= -1;
@@ -32,4 +40,22 @@ document.addEventListener("DOMContentLoaded", () => {
       dy *= -1;
     }
   }, 1);
+
+  let paddleY = 0;
+  let pdy = 10; // Paddle displacement in y direction
+
+  document.addEventListener("keydown", (event) => {
+    event.preventDefault(); // To prevent default behaviour of arrow keys
+    if (
+      event.keyCode === 40 &&
+      paddleY < table.offsetHeight - paddle.offsetHeight
+    ) {
+      // Down arrow
+      paddleY += pdy;
+    } else if (event.keyCode === 38 && paddleY > 0) {
+      // Up arrow
+      paddleY -= pdy;
+    }
+    paddle.style.top = `${paddleY}px`;
+  });
 });
